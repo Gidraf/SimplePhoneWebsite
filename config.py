886e892e0be793ab335e486e88bd39a38a6c9cfe
* <orenjagidraf@gmail.com>
2020-01-19 18:59:23 +0300

    add user to db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config():
-    SECRET_KEY=os.environ.get("SECRET_KEY)
+    SECRET_KEY=os.environ.get("SECRET_KEY")
--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template,request,jsonify
+from models.user import User
 from models import db
 import os
 from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config.from_object(Config)
 
 db.init_app(app)
 
@@ -15,8 +17,17 @@ migrate = Migrate(app, db)
 def home():
     if request.method == "GET":
         return render_template("index.html")
-    password =  request.form.get("password")
-    return jsonify(password)
+    password =  request.values.get("password")
+    username = request.values.get("username")
+    email = request.values.get("email")
+    user = User()
+    user.password = password
+    user.username = username
+    user.email = email
+    db.session.add(user)
+    db.session.commit()
+    results = User.query.filter_by(email=email).first()
+    return jsonify({"username":results.username})
 
 
 if __name__ == "__main__":
